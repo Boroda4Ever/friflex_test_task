17e1d57aa7b3b3ccddd137ed8ef5cee90a6b22b9
Boroda4Ever <andrey.borodin.2002@mail.ru>
2023-01-15 11:33:34 +0300

    change appId

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 //Строковые константы для хранения частей запроса к API
 class APIConstants {
-  static const appId = '672969dcda3d656a6b8594a928570ab4';
+  static const appId = '1369dd6b5ae78fc9952261ab9aa236b4';
   static const baseScheme = 'https://';
   static const baseUrlDomain = 'api.openweathermap.org';
   static const forecastPath = '/data/2.5/forecast/daily';
